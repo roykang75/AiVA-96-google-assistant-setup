0c92a08fb300a48c0600dfe56adfd624e3981de6
Roy Kang (Sungtae, Kang) <roykang75@gmail.com>
2018-09-18 16:17:55 +0900

    Update rpi.sh
    Update rpi.sh

--- rpi.sh
+++ rpi.sh
@@ -2,7 +2,7 @@
 
 echo "========== Installing python3 and Libraries ==========="
 sudo apt-get install -y vim vsftpd
-sudo apt-get install portaudio19-dev libffi-dev libssl-dev libmpg123-dev
+sudo apt-get install -y portaudio19-dev libffi-dev libssl-dev libmpg123-dev
 sudo apt-get install -y python3 python3-dev python3-venv
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools wheel
