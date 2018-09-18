eda608087b8b367fa36f35565487c98af29546c2
Roy, Kang <stkang@inslab.co.kr>
2018-09-18 17:04:31 +0900

    1111

--- rpi.sh
+++ rpi.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 echo "========== Installing python3 and Libraries ==========="
-<<<<<<< HEAD
-sudo apt-get install -y vim vsftpd pulseaudio
-sudo apt-get install portaudio19-dev libffi-dev libssl-dev libmpg123-dev
-=======
 sudo apt-get install -y vim vsftpd
 sudo apt-get install -y portaudio19-dev libffi-dev libssl-dev libmpg123-dev
->>>>>>> 0c92a08fb300a48c0600dfe56adfd624e3981de6
 sudo apt-get install -y python3 python3-dev python3-venv
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools wheel
